09157d609a5fa4156929c960b6999fd80f725c4d
Jobayer Ahmed <Jobayer6735@gmail.com>
2024-11-29 22:29:38 +0600

    Implemented github rate limit feature

--- server/helpers/octokitClient.js
+++ server/helpers/octokitClient.js
@@ -7,27 +7,21 @@ export const createOctokitInstance = (accessToken) => {
     return new MyOctokit({
         auth: accessToken,
         throttle: {
-            onRateLimit: (retryAfter, options) => {
-                console.warn(`Rate limit hit for request ${options.method} ${options.url}`);
-                if (options.request.retryCount === 0) {
-                    console.log(`Retrying after ${retryAfter} seconds...`);
-                    return true; // Retry once
-                } else {
-                    console.log(`Retry limit reached. Not retrying further.`);
-                    return false; // Do not retry again
+            onRateLimit: (retryAfter, options, octokit, retryCount) => {
+                octokit.log.warn(
+                    `Request quota exhausted for request ${options.method} ${options.url}`,
+                );
+                // only retries once
+                if (retryCount < 1) {
+                    octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                    return true;
                 }
             },
-            onSecondaryRateLimit: (retryAfter, options) => {
-                console.warn(`Secondary rate limit hit for ${options.method} ${options.url}`);
-                
-                // Check if the request has already been retried
-                if (options.request.retryCount === 0) {
-                    console.log(`Retrying after ${retryAfter} seconds...`);
-                    return true; // Retry the request
-                } else {
-                    console.log(`Retry limit reached. Not retrying further.`);
-                    return false; // Do not retry again
-                }
+            onSecondaryRateLimit: (retryAfter, options, octokit) => {
+                // does not retry, only logs a warning
+                octokit.log.warn(
+                    `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+                );
             },
         },
     });
